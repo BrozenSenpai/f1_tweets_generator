11be257702e29d3373d426530e5b99fc9c6d29c3
brozen <szymon.mazurkievicz@gmail.com>
2021-12-11 18:45:24 +0100

    scheduling

--- src/etl/utils/config.py
+++ src/etl/utils/config.py
@@ -15,5 +15,5 @@ import os
 consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
 access_token = os.environ['ACCESS_TOKEN']
-access_token_secret = os.environ['ACESS_TOKEN_SECRET']
+access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 db_path = os.environ['DB_PATH']
